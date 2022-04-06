c58f12545e8f95f10c606c4b5ae86199e441d68a
chiragdevlani35 <80832060+chiragdevlani35@users.noreply.github.com>
2022-04-06 21:53:35 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,7 +14,7 @@ setcred({...cred,[event.target.name]:event.target.value});
         event.preventDefault();
         console.log(cred.email+" "+cred.password);
        
-            const response=await fetch(`http://localhost:5000/auth/login`,{
+            const response=await fetch(`http://localhost:3000/auth/login`,{
                 method:'POST',
                 headers:{
                     'Content-type':'application/json'
